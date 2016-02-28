c219a752d06fcfd5d6f9f0c0cff23711707511a9
kenzhang <kenverygood75@yahoo.ca>
2016-02-27 23:01:24 -0800

    ken's item edits

--- app/models/Item.java
+++ app/models/Item.java
@@ -7,7 +7,16 @@ public class Item {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-	public String id;
-
+	/*public String id;
+	public String name;*/
+	
+	public Int id;
     public String name;
+    public Int startDate;
+    public Int endDate;
+    public String street;
+    public String city;
+    public String postalCode;
+    public String description;
+    public String title;
 }
